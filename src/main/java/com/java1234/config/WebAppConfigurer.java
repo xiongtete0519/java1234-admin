07cd3da055fa75b4288f0cf0c03520ce0b4fe791
CodeLeader <1144722582@qq.com>
2023-02-21 11:25:04 +0800

    用户头像显示(目前只是映射到本地文件，其实可以使用oss存储头像)

--- src/main/java/com/java1234/config/WebAppConfigurer.java
+++ src/main/java/com/java1234/config/WebAppConfigurer.java
@@ -2,6 +2,7 @@ package com.java1234.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //解决跨域
@@ -15,6 +16,11 @@ public class WebAppConfigurer implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE","OPTIONS")
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/userAvatar/**").addResourceLocations("file:F:\\vscode_test\\java1234-admin-front\\src\\userAvatar\\");
     }
 
   
