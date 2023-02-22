234fcca2e9d96751dd13db4afbc95a03795d9ba3
CodeLeader <1144722582@qq.com>
2023-02-22 21:06:57 +0800

    角色管理-条件分页查询角色信息

--- src/main/java/com/java1234/controller/SysRoleController.java
+++ src/main/java/com/java1234/controller/SysRoleController.java
@@ -1,13 +1,15 @@
 package com.java1234.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.java1234.entity.PageBean;
 import com.java1234.entity.R;
 import com.java1234.entity.SysRole;
 import com.java1234.service.SysRoleService;
+import com.java1234.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -27,6 +29,19 @@ public class SysRoleController {
         Map<String,Object> resultMap=new HashMap<>();
         List<SysRole> roleList = sysRoleService.list();
         resultMap.put("roleList",roleList);
+        return R.ok(resultMap);
+    }
+
+    //根据条件分页查询角色信息
+    @PostMapping("/list")
+    @PreAuthorize("hasAuthority('system:role:query')")
+    public R list(@RequestBody PageBean pageBean){
+        String query=pageBean.getQuery().trim();
+        Page<SysRole> pageResult = sysRoleService.page(new Page<>(pageBean.getPageNum(), pageBean.getPageSize()),new QueryWrapper<SysRole>().like(StringUtil.isNotEmpty(query),"name",query));
+        List<SysRole> userList = pageResult.getRecords();
+        Map<String,Object> resultMap=new HashMap<>();
+        resultMap.put("roleList",userList);
+        resultMap.put("total",pageResult.getTotal());
         return R.ok(resultMap);
     }
 }
