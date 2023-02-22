89a5ebce89fc7ab752e7a146d8eb306ab93d112e
CodeLeader <1144722582@qq.com>
2023-02-22 15:35:46 +0800

    头像更换功能实现

--- src/main/java/com/java1234/controller/SysUserController.java
+++ src/main/java/com/java1234/controller/SysUserController.java
@@ -3,15 +3,23 @@ package com.java1234.controller;
 import com.java1234.entity.R;
 import com.java1234.entity.SysUser;
 import com.java1234.service.SysUserService;
+import com.java1234.util.DateUtil;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/sys/user")
@@ -22,6 +30,8 @@ public class SysUserController {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Value("${avatarImagesFilePath}")
+    private String avatarImagesFilePath;
 
     //更新个人信息
     @PostMapping("/save")
@@ -49,5 +59,40 @@ public class SysUserController {
         }
 
     }
+    /**
+     * 上传用户头像图片
+     */
+    @RequestMapping("/uploadImage")
+    @PreAuthorize("hasAuthority('system:user:edit')")
+    public Map<String,Object> uploadImage(MultipartFile file)throws Exception{
+        Map<String,Object> resultMap=new HashMap<>();
+        if(!file.isEmpty()){
+            // 获取文件名
+            String originalFilename = file.getOriginalFilename();
+            String suffixName=originalFilename.substring(originalFilename.lastIndexOf("."));
+            String newFileName= DateUtil.getCurrentDateStr()+suffixName;
+            FileUtils.copyInputStreamToFile(file.getInputStream(),new File(avatarImagesFilePath+newFileName));
+            resultMap.put("code",0);
+            resultMap.put("msg","上传成功");
+            Map<String,Object> dataMap=new HashMap<>();
+            dataMap.put("title",newFileName);
+            dataMap.put("src","image/userAvatar/"+newFileName);
+            resultMap.put("data",dataMap);
+        }
+        return resultMap;
+    }
 
+    /**
+     * 修改用户头像
+     * @param sysUser
+     * @return
+     */
+    @RequestMapping("/updateAvatar")
+    @PreAuthorize("hasAuthority('system:user:edit')")
+    public R updateAvatar(@RequestBody SysUser sysUser){
+        SysUser currentUser = sysUserService.getById(sysUser.getId());
+        currentUser.setAvatar(sysUser.getAvatar());
+        sysUserService.updateById(currentUser);
+        return R.ok();
+    }
 }
