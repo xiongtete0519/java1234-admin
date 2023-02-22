0333cb5c9cacc59e66e827cffac2b81bf5590089
CodeLeader <1144722582@qq.com>
2023-02-22 15:13:30 +0800

    MybatisPlus自动填充配置

---
+++ src/main/java/com/java1234/config/MyMetaObjectHandler.java
@@ -0,0 +1,29 @@
+package com.java1234.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * MybatisPlus自动填充
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill...");
+        //这里写属性名称不是字段名称
+        this.setFieldValByName("createTime",new Date(),metaObject);
+        this.setFieldValByName("updateTime",new Date(),metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill...");
+        this.setFieldValByName("updateTime",new Date(),metaObject);
+    }
+}
--- src/main/java/com/java1234/entity/BaseEntity.java
+++ src/main/java/com/java1234/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.java1234.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
@@ -22,6 +23,7 @@ public class BaseEntity implements Serializable {
     /**
      * 创建日期
      */
+    @TableField(fill = FieldFill.INSERT)
     @JsonSerialize(using=CustomDateTimeSerializer.class)
     @JsonFormat(pattern ="yyyy-MM-dd HH:mm:ss")
     private Date createTime;
@@ -29,6 +31,7 @@ public class BaseEntity implements Serializable {
     /**
      * 更新日期
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     @JsonSerialize(using=CustomDateTimeSerializer.class)
     @JsonFormat(pattern ="yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
