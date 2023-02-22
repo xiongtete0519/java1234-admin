33c90452635d8305e7676740e1170a360bd94da8
CodeLeader <1144722582@qq.com>
2023-02-22 23:39:25 +0800

    菜单管理-添加修改功能实现

--- src/main/java/com/java1234/controller/SysMenuController.java
+++ src/main/java/com/java1234/controller/SysMenuController.java
@@ -6,10 +6,11 @@ import com.java1234.entity.SysMenu;
 import com.java1234.service.SysMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 系统菜单控制器
@@ -27,5 +28,27 @@ public class SysMenuController {
     public R list(){
         List<SysMenu> menuList = sysMenuService.list(new QueryWrapper<SysMenu>().orderByAsc("order_num"));
         return R.ok().put("treeMenu",sysMenuService.buildTreeMenu(menuList));
+    }
+
+    //添加或者修改
+    @PostMapping("/save")
+    @PreAuthorize("hasAnyAuthority('system:menu:add','system:menu:edit')")
+    public R save(@RequestBody SysMenu sysMenu){
+        if(sysMenu.getId()==null || sysMenu.getId()==-1){   //添加
+            sysMenuService.save(sysMenu);
+        }else{  //修改
+            sysMenuService.updateById(sysMenu);
+        }
+        return R.ok();
+    }
+
+    //根据id查询
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('system:menu:query')")
+    public R findById(@PathVariable(value = "id")Long id){
+        SysMenu sysMenu = sysMenuService.getById(id);
+        Map<String,Object> map=new HashMap<>();
+        map.put("sysMenu",sysMenu);
+        return R.ok(map);
     }
 }
