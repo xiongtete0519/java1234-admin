749bf06b8082fef46377ef6f4d9ac690b596c6a3
CodeLeader <1144722582@qq.com>
2023-02-23 22:53:49 +0800

    权限加载实现Redis缓存

--- src/main/java/com/java1234/common/constant/Constant.java
+++ src/main/java/com/java1234/common/constant/Constant.java
@@ -1,5 +1,8 @@
 package com.java1234.common.constant;
 
 public class Constant {
+
     public final static String DEFAULT_PASSWORD="123456";
+
+    public final static String AUTHORITY_KEY="authority";
 }
--- src/main/java/com/java1234/service/impl/SysUserServiceImpl.java
+++ src/main/java/com/java1234/service/impl/SysUserServiceImpl.java
@@ -2,6 +2,7 @@ package com.java1234.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.java1234.common.constant.Constant;
 import com.java1234.entity.SysMenu;
 import com.java1234.entity.SysRole;
 import com.java1234.entity.SysUser;
@@ -9,7 +10,9 @@ import com.java1234.mapper.SysMenuMapper;
 import com.java1234.mapper.SysRoleMapper;
 import com.java1234.service.SysUserService;
 import com.java1234.mapper.SysUserMapper;
+import com.java1234.util.RedisUtil;
 import com.java1234.util.StringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +25,7 @@ import java.util.stream.Collectors;
 /**
  *
  */
+@Slf4j
 @Service
 public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser>
 implements SysUserService{
@@ -31,6 +35,9 @@ implements SysUserService{
 
     @Autowired
     private SysMenuMapper sysMenuMapper;
+
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Override
     public SysUser getByUsername(String username) {
@@ -43,40 +50,50 @@ implements SysUserService{
     @Override
     public String getUserAuthorityInfo(Long userId) {
         StringBuffer authority=new StringBuffer();
-        //根据用户id获取所有的角色信息
-        List<SysRole> roleList = sysRoleMapper.selectList(
-                new QueryWrapper<SysRole>()
-                        .inSql("id", "select role_id from sys_user_role where user_id=" + userId)
-        );
-        if(roleList.size()>0){
-            // 格式ROLE_admin,ROLE_common
-            String roleCodeStrs = roleList.stream().map(r -> "ROLE_" + r.getCode()).collect(Collectors.joining(","));
-            authority.append(roleCodeStrs);
-        }
-        //遍历所有的角色，获取所有菜单权限，而且不重复
-        Set<String> menuCodeSet=new HashSet<>();
-        for (SysRole sysRole : roleList) {
-            //每个角色的菜单权限
-            List<SysMenu> sysMenuList = sysMenuMapper.selectList(
-                    new QueryWrapper<SysMenu>()
-                            .inSql("id", "select menu_id from sys_role_menu where role_id=" + sysRole.getId())
 
+        if(redisUtil.hasKey(Constant.AUTHORITY_KEY+userId)){
+            log.info("缓存命中");
+            authority.append(redisUtil.get(Constant.AUTHORITY_KEY,String.valueOf(userId)));
+            System.out.println("缓存命中："+redisUtil.get(Constant.AUTHORITY_KEY,String.valueOf(userId)));
+        }else{
+            log.info("查数据库");
+            //根据用户id获取所有的角色信息
+            List<SysRole> roleList = sysRoleMapper.selectList(
+                    new QueryWrapper<SysRole>()
+                            .inSql("id", "select role_id from sys_user_role where user_id=" + userId)
             );
-            //取出菜单权限编码
-            for (SysMenu sysMenu : sysMenuList) {
-                String perms = sysMenu.getPerms();
-                if(StringUtil.isNotEmpty(perms)){
-                    menuCodeSet.add(perms);
+            if(roleList.size()>0){
+                // 格式ROLE_admin,ROLE_common
+                String roleCodeStrs = roleList.stream().map(r -> "ROLE_" + r.getCode()).collect(Collectors.joining(","));
+                authority.append(roleCodeStrs);
+            }
+            //遍历所有的角色，获取所有菜单权限，而且不重复
+            Set<String> menuCodeSet=new HashSet<>();
+            for (SysRole sysRole : roleList) {
+                //每个角色的菜单权限
+                List<SysMenu> sysMenuList = sysMenuMapper.selectList(
+                        new QueryWrapper<SysMenu>()
+                                .inSql("id", "select menu_id from sys_role_menu where role_id=" + sysRole.getId())
+
+                );
+                //取出菜单权限编码
+                for (SysMenu sysMenu : sysMenuList) {
+                    String perms = sysMenu.getPerms();
+                    if(StringUtil.isNotEmpty(perms)){
+                        menuCodeSet.add(perms);
+                    }
                 }
             }
+            //将权限编码设置到authority里面
+            if(menuCodeSet.size()>0){
+                authority.append(",");
+                String menuCodeStrs = menuCodeSet.stream().collect(Collectors.joining(","));
+                authority.append(menuCodeStrs);
+            }
+            //将权限字符串存入redis
+            redisUtil.set(Constant.AUTHORITY_KEY,String.valueOf(userId),authority,10*60);
+            log.info("authority={}",authority.toString());
         }
-        //将权限编码设置到authority里面
-        if(menuCodeSet.size()>0){
-            authority.append(",");
-            String menuCodeStrs = menuCodeSet.stream().collect(Collectors.joining(","));
-            authority.append(menuCodeStrs);
-        }
-        System.out.println("authority="+authority.toString());
         return authority.toString();
     }
 }
